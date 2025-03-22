24b33cc4ddd80d493710bad1159fc3e02c744f00
marcelweikum <marcel.weikum@wesodev.de>
2025-03-22 12:59:55 +0100

    Added log-levels exercise

---
+++ cpp/log-levels/log_levels.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+namespace log_line {
+std::string message(std::string line) {
+    std::string::size_type separator{line.find(" ")};
+    return line.substr(separator + 1);
+}
+
+std::string log_level(std::string line) {
+    std::string::size_type level_start{line.find("[") + 1};
+    std::string::size_type level_end{line.find("]")};
+    return line.substr(level_start, level_end - level_start);
+}
+
+std::string reformat(std::string line) {
+    return log_line::message(line) + " (" + log_line::log_level(line) + ")";
+}
+}  // namespace log_line
