eac3b2e211db71ba3919aba4a602fea50fcaad9e
marcelweikum <marcel.weikum@wesodev.de>
2025-03-22 20:05:01 +0100

    Solved making-the-grade exercise

--- cpp/making-the-grade/making_the_grade.cpp
+++ cpp/making-the-grade/making_the_grade.cpp
@@ -3,34 +3,67 @@
 #include <vector>
 
 // Round down all provided student scores.
-std::vector<int> round_down_scores(std::vector<double> student_scores) {
-    // TODO: Implement round_down_scores
-    return {};
+std::vector<int> round_down_scores(std::vector<double> student_scores)
+{
+    std::vector<int> rounded_scores;
+    for (double score : student_scores)
+    {
+        score = static_cast<int>(score);
+        rounded_scores.push_back(score);
+    }
+    return rounded_scores;
 }
 
 // Count the number of failing students out of the group provided.
-int count_failed_students(std::vector<int> student_scores) {
-    // TODO: Implement count_failed_students
-    return 0;
+int count_failed_students(std::vector<int> student_scores)
+{
+    int failed_students{0};
+    for (int score : student_scores)
+    {
+        if (score <= 40)
+        {
+            failed_students++;
+        }
+    }
+    return failed_students;
 }
 
 // Create a list of grade thresholds based on the provided highest grade.
-std::array<int, 4> letter_grades(int highest_score) {
-    // TODO: Implement letter_grades
-    return {};
+std::array<int, 4> letter_grades(int highest_score)
+{
+    std::array<int, 4> grade_thresholds;
+    int total_range = highest_score - 40;
+    int range = total_range / 4;
+    for (int i{0}; i < 4; i++)
+    {
+        grade_thresholds[i] = 41 + (range * i);
+    }
+    return grade_thresholds;
 }
 
 // Organize the student's rank, name, and grade information in ascending order.
 std::vector<std::string> student_ranking(
-    std::vector<int> student_scores, std::vector<std::string> student_names) {
-    // TODO: Implement student_ranking
-    return {};
+    std::vector<int> student_scores, std::vector<std::string> student_names)
+{
+    std::vector<std::string> student_ranking;
+    for (int i{0}; i < student_scores.size(); i++)
+    {
+        student_ranking.push_back(std::to_string(i + 1) + ". " + student_names[i] + ": " + std::to_string(student_scores[i]));
+    }
+    return student_ranking;
 }
 
 // Create a string that contains the name of the first student to make a perfect
 // score on the exam.
 std::string perfect_score(std::vector<int> student_scores,
-                          std::vector<std::string> student_names) {
-    // TODO: Implement perfect_score
+                          std::vector<std::string> student_names)
+{
+    for (int i{0}; i < student_scores.size(); i++)
+    {
+        if (student_scores[i] == 100)
+        {
+            return student_names[i];
+        }
+    }
     return "";
 }
--- cpp/making-the-grade/making_the_grade_test.cpp
+++ cpp/making-the-grade/making_the_grade_test.cpp
@@ -7,7 +7,8 @@
 
 using namespace std;
 
-TEST_CASE("Check correct mark conversion (empty)", "[task_1]") {
+TEST_CASE("Check correct mark conversion (empty)", "[task_1]")
+{
     vector<double> input{};
     vector<int> expected{};
     vector<int> actual = round_down_scores(input);
@@ -15,9 +16,8 @@ TEST_CASE("Check correct mark conversion (empty)", "[task_1]") {
     REQUIRE(expected == actual);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-
-TEST_CASE("Check correct mark conversion (all < 0.5)", "[task_1]") {
+TEST_CASE("Check correct mark conversion (all < 0.5)", "[task_1]")
+{
     vector<double> input{5.2, 77.1, 91.0};
     vector<int> expected{5, 77, 91};
     vector<int> actual = round_down_scores(input);
@@ -25,7 +25,8 @@ TEST_CASE("Check correct mark conversion (all < 0.5)", "[task_1]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Check correct mark conversion (all mixed)", "[task_1]") {
+TEST_CASE("Check correct mark conversion (all mixed)", "[task_1]")
+{
     vector<double> input{5.2, 77.1, 91.0, 42.9, 11.6544};
     vector<int> expected{5, 77, 91, 42, 11};
     vector<int> actual = round_down_scores(input);
@@ -33,7 +34,8 @@ TEST_CASE("Check correct mark conversion (all mixed)", "[task_1]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Count failed students (none failed)", "[task_2]") {
+TEST_CASE("Count failed students (none failed)", "[task_2]")
+{
     vector<int> input{89, 85, 42, 57, 90, 100, 95, 48, 70, 96};
     int expected{0};
     int actual = count_failed_students(input);
@@ -41,7 +43,8 @@ TEST_CASE("Count failed students (none failed)", "[task_2]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Count failed students (some failed)", "[task_2]") {
+TEST_CASE("Count failed students (some failed)", "[task_2]")
+{
     vector<int> input{40, 40, 35, 70, 30, 41, 90};
     int expected{4};
     int actual = count_failed_students(input);
@@ -49,21 +52,24 @@ TEST_CASE("Count failed students (some failed)", "[task_2]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Test letter grades: 100", "[task_3]") {
+TEST_CASE("Test letter grades: 100", "[task_3]")
+{
     int input{100};
     array<int, 4> expected{41, 56, 71, 86};
     array<int, 4> actual = letter_grades(input);
 
     REQUIRE(expected == actual);
 }
-TEST_CASE("Test letter grades: 97", "[task_3]") {
+TEST_CASE("Test letter grades: 97", "[task_3]")
+{
     int input{97};
     array<int, 4> expected{41, 55, 69, 83};
     array<int, 4> actual = letter_grades(input);
 
     REQUIRE(expected == actual);
 }
-TEST_CASE("Test letter grades: 81", "[task_3]") {
+TEST_CASE("Test letter grades: 81", "[task_3]")
+{
     int input{81};
     array<int, 4> expected{41, 51, 61, 71};
     array<int, 4> actual = letter_grades(input);
@@ -71,7 +77,8 @@ TEST_CASE("Test letter grades: 81", "[task_3]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Rank one student", "[task_4]") {
+TEST_CASE("Rank one student", "[task_4]")
+{
     vector<int> grades{82};
     vector<string> names{"Betty"};
     vector<string> expected{"1. Betty: 82"};
@@ -80,46 +87,51 @@ TEST_CASE("Rank one student", "[task_4]") {
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Rank several student", "[task_4]") {
+TEST_CASE("Rank several student", "[task_4]")
+{
     vector<int> grades{100, 98, 92, 86, 70, 68, 67, 60};
-    vector<string> names{"Rui",  "Betty", "Joci", "Yoshi",
-                         "Kora", "Bern",  "Jan",  "Rose"};
-    vector<string> expected{"1. Rui: 100",  "2. Betty: 98", "3. Joci: 92",
-                            "4. Yoshi: 86", "5. Kora: 70",  "6. Bern: 68",
-                            "7. Jan: 67",   "8. Rose: 60"};
+    vector<string> names{"Rui", "Betty", "Joci", "Yoshi",
+                         "Kora", "Bern", "Jan", "Rose"};
+    vector<string> expected{"1. Rui: 100", "2. Betty: 98", "3. Joci: 92",
+                            "4. Yoshi: 86", "5. Kora: 70", "6. Bern: 68",
+                            "7. Jan: 67", "8. Rose: 60"};
     vector<string> actual = student_ranking(grades, names);
 
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("No perfect score", "[task_5]") {
+TEST_CASE("No perfect score", "[task_5]")
+{
     vector<int> grades{11, 34, 92, 23, 70, 76, 67, 60};
-    vector<string> names{"Rui",  "Betty", "Joci", "Yoshi",
-                         "Kora", "Bern",  "Jan",  "Rose"};
+    vector<string> names{"Rui", "Betty", "Joci", "Yoshi",
+                         "Kora", "Bern", "Jan", "Rose"};
     string expected{""};
     string actual = perfect_score(grades, names);
 
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("One perfect score", "[task_5]") {
+TEST_CASE("One perfect score", "[task_5]")
+{
     vector<int> grades{11, 34, 92, 23, 70, 76, 67, 100};
-    vector<string> names{"Rui",  "Betty", "Joci", "Yoshi",
-                         "Kora", "Bern",  "Jan",  "Rose"};
+    vector<string> names{"Rui", "Betty", "Joci", "Yoshi",
+                         "Kora", "Bern", "Jan", "Rose"};
     string expected{"Rose"};
     string actual = perfect_score(grades, names);
 
     REQUIRE(expected == actual);
 }
 
-TEST_CASE("Several perfect scores", "[task_5]") {
+TEST_CASE("Several perfect scores", "[task_5]")
+{
     vector<int> grades{11, 100, 92, 23, 70, 100, 67, 100};
-    vector<string> names{"Rui",  "Betty", "Joci", "Yoshi",
-                         "Kora", "Bern",  "Jan",  "Rose"};
+    vector<string> names{"Rui", "Betty", "Joci", "Yoshi",
+                         "Kora", "Bern", "Jan", "Rose"};
     string expected{"Betty"};
     string actual = perfect_score(grades, names);
 
     REQUIRE(expected == actual);
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
