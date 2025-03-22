9e713cc2e4a81e659ba43bfa1bd3a61bc502e24d
marcelweikum <marcel.weikum@wesodev.de>
2025-03-22 20:20:23 +0100

    Solved ellens-alien-game exercise

--- cpp/ellens-alien-game/ellens_alien_game.cpp
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -1,4 +1,58 @@
-namespace targets {
-// TODO: Insert the code for the alien class here
+namespace targets
+{
+    class Alien
+    {
+    public:
+        Alien(int x, int y)
+        {
+            x_coordinate = x;
+            y_coordinate = y;
+        }
 
-}  // namespace targets
+        int get_health()
+        {
+            return health;
+        }
+
+        bool hit()
+        {
+            if (health > 0)
+            {
+                health -= 1;
+                return true;
+            }
+            return false;
+        }
+
+        bool is_alive()
+        {
+            if (health > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool teleport(int x_new, int y_new)
+        {
+            x_coordinate = x_new;
+            y_coordinate = y_new;
+            return true;
+        }
+
+        bool collision_detection(Alien alien)
+        {
+            if (alien.x_coordinate == x_coordinate || alien.y_coordinate == y_coordinate)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        int x_coordinate{};
+        int y_coordinate{};
+
+    private:
+        int health{3};
+    };
+} // namespace targets
--- cpp/ellens-alien-game/ellens_alien_game_test.cpp
+++ cpp/ellens-alien-game/ellens_alien_game_test.cpp
@@ -7,34 +7,37 @@
 
 using namespace targets;
 
-TEST_CASE("Alien has correct initial coordinates", "[task_1]") {
+TEST_CASE("Alien has correct initial coordinates", "[task_1]")
+{
     Alien alien{2, -1};
     REQUIRE(alien.x_coordinate == 2);
     REQUIRE(alien.y_coordinate == -1);
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-
-TEST_CASE("Alien has correct initial health", "[task_1]") {
+TEST_CASE("Alien has correct initial health", "[task_1]")
+{
     Alien alien{22, 0};
     REQUIRE(alien.get_health() == 3);
 }
 
-TEST_CASE("New alien does not share old aliens position", "[task_1]") {
+TEST_CASE("New alien does not share old aliens position", "[task_1]")
+{
     Alien alien1{22, 0};
     Alien alien2{0, 22};
     REQUIRE(alien1.x_coordinate != alien2.x_coordinate);
     REQUIRE(alien1.y_coordinate != alien2.y_coordinate);
 }
 
-TEST_CASE("Alien is always hit", "[task_2]") {
+TEST_CASE("Alien is always hit", "[task_2]")
+{
     Alien alien{6, 7};
     REQUIRE(alien.hit());
 }
 
 TEST_CASE(
     "Alien is alive while health is greater than 0 and stays dead afterwards",
-    "[task_3]") {
+    "[task_3]")
+{
     Alien alien{2, 54};
     REQUIRE(alien.is_alive());
     alien.hit();
@@ -48,12 +51,14 @@ TEST_CASE(
     REQUIRE(alien.get_health() == 0);
 }
 
-TEST_CASE("Alien Teleports reports succesful", "[task_4]") {
+TEST_CASE("Alien Teleports reports succesful", "[task_4]")
+{
     Alien alien{22, 1};
     REQUIRE(alien.teleport(99, 8));
 }
 
-TEST_CASE("Alien Teleports where it should", "[task_4]") {
+TEST_CASE("Alien Teleports where it should", "[task_4]")
+{
     Alien alien{2, -54};
     REQUIRE(alien.x_coordinate == 2);
     REQUIRE(alien.y_coordinate == -54);
@@ -62,7 +67,8 @@ TEST_CASE("Alien Teleports where it should", "[task_4]") {
     REQUIRE(alien.y_coordinate == 8);
 }
 
-TEST_CASE("Alien collision detection with other aliens", "[task_5]") {
+TEST_CASE("Alien collision detection with other aliens", "[task_5]")
+{
     Alien alien1{22, 0};
     Alien alien2{0, 22};
     Alien alien3{22, 0};
@@ -70,4 +76,5 @@ TEST_CASE("Alien collision detection with other aliens", "[task_5]") {
     REQUIRE(alien1.collision_detection(alien3));
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 #endif
