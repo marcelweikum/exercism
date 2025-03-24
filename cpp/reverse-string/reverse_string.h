598f8e9f4c968c420af7e4efea1b34ea32934a6d
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-24 10:35:16 +0100

    Added reverse-string exercise

---
+++ cpp/reverse-string/reverse_string.h
@@ -0,0 +1,10 @@
+#if !defined(REVERSE_STRING_H)
+#define REVERSE_STRING_H
+
+namespace reverse_string {
+
+// TODO: add your solution here
+
+}  // namespace reverse_string
+
+#endif  // REVERSE_STRING_H
