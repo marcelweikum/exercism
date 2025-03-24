b0b9420060babd4c2339dd6fafcf60711422afe9
marcelweikum <marcel.weikum@wesodev.de>
2025-03-24 19:59:52 +0100

    Added rna-transcription exercise

---
+++ cpp/rna-transcription/rna_transcription.cpp
@@ -0,0 +1,7 @@
+#include "rna_transcription.h"
+
+namespace rna_transcription {
+
+// TODO: add your solution here
+
+}  // namespace rna_transcription
---
+++ cpp/rna-transcription/rna_transcription.h
@@ -0,0 +1,10 @@
+#if !defined(RNA_TRANSCRIPTION_H)
+#define RNA_TRANSCRIPTION_H
+
+namespace rna_transcription {
+
+// TODO: add your solution here
+
+}  // namespace rna_transcription
+
+#endif  // RNA_TRANSCRIPTION_H
