9ac6785be342774c46678741df61b8353530e241
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-24 11:41:21 +0100

    Solved space-age exercise

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -1,10 +1,27 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
 
-namespace space_age {
+namespace space_age
+{
+    class space_age
+    {
+    public:
+        space_age(long seconds);
 
-// TODO: add your solution here
+        long seconds() const;
 
-}  // namespace space_age
+        double on_earth() const;
+        double on_mercury() const;
+        double on_venus() const;
+        double on_mars() const;
+        double on_jupiter() const;
+        double on_saturn() const;
+        double on_uranus() const;
+        double on_neptune() const;
 
-#endif  // SPACE_AGE_H
+    private:
+        long seconds_{};
+    };
+} // namespace space_age
+
+#endif // SPACE_AGE_H
