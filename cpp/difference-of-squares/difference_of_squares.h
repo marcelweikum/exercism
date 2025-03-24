be2d03d18aa0f998a651670cffd383dd7b500782
marcelweikum <marcel.weikum@wesodev.de>
2025-03-24 20:17:20 +0100

    Added difference-of-squares exercise

---
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -0,0 +1,10 @@
+#if !defined(DIFFERENCE_OF_SQUARES_H)
+#define DIFFERENCE_OF_SQUARES_H
+
+namespace difference_of_squares {
+
+// TODO: add your solution here
+
+}  // namespace difference_of_squares
+
+#endif  // DIFFERENCE_OF_SQUARES_H
