ccc3ae034d9961a6003a254d6bdfcb3d8e16f20d
marcelweikum <marcel.weikum@wesodev.de>
2025-03-24 20:10:57 +0100

    Added collatz-conjecture exercise

---
+++ cpp/collatz-conjecture/collatz_conjecture.h
@@ -0,0 +1,10 @@
+#if !defined(COLLATZ_CONJECTURE_H)
+#define COLLATZ_CONJECTURE_H
+
+namespace collatz_conjecture {
+
+// TODO: add your solution here
+
+}  // namespace collatz_conjecture
+
+#endif  // COLLATZ_CONJECTURE_H
