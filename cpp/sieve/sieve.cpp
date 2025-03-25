7c9557ca04e08901b8101d2aa8a094e9e13ea407
marcelweikum <marcel.weikum@wesodev.de>
2025-03-25 13:33:55 +0100

    Added sieve exercise

---
+++ cpp/sieve/sieve.cpp
@@ -0,0 +1,7 @@
+#include "sieve.h"
+
+namespace sieve {
+
+// TODO: add your solution here
+
+}  // namespace sieve
---
+++ cpp/sieve/sieve.h
@@ -0,0 +1,10 @@
+#if !defined(SIEVE_H)
+#define SIEVE_H
+
+namespace sieve {
+
+// TODO: add your solution here
+
+}  // namespace sieve
+
+#endif  // SIEVE_H
