b9ec393f58554076c4aaa833947f09398ea3ac31
marcelweikum <marcel.weikum@wesodev.de>
2025-03-25 13:40:00 +0100

    Added say exercise

---
+++ cpp/say/say.cpp
@@ -0,0 +1,7 @@
+#include "say.h"
+
+namespace say {
+
+// TODO: add your solution here
+
+}  // namespace say
---
+++ cpp/say/say.h
@@ -0,0 +1,10 @@
+#if !defined(SAY_H)
+#define SAY_H
+
+namespace say {
+
+// TODO: add your solution here
+
+}  // namespace say
+
+#endif  // SAY_H
