87f96dfbeb9f1ca1b3abb1577fe0fad50023c8f9
marcelweikum <marcel.weikum@wesodev.de>
2025-03-25 15:29:05 +0100

    Solved allergies exercise

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -1,7 +1,29 @@
 #include "allergies.h"
 
-namespace allergies {
+namespace allergies
+{
+    allergy_test::allergy_test(int i)
+    {
+        static const std::vector<std::string> allergy_list = {
+            "eggs", "peanuts", "shellfish", "strawberries",
+            "tomatoes", "chocolate", "pollen", "cats"};
 
-// TODO: add your solution here
+        for (int bit = 0; bit < 8; ++bit)
+        {
+            if (i & (1 << bit))
+            {
+                allergies_.insert(allergy_list[bit]);
+            }
+        }
+    }
 
-}  // namespace allergies
+    bool allergy_test::is_allergic_to(std::string item)
+    {
+        return allergies_.find(item) != allergies_.end();
+    }
+
+    std::unordered_set<std::string> allergy_test::get_allergies()
+    {
+        return allergies_;
+    }
+} // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -1,10 +1,25 @@
 #if !defined(ALLERGIES_H)
 #define ALLERGIES_H
 
-namespace allergies {
+#include <vector>
+#include <string>
+#include <unordered_set>
 
-// TODO: add your solution here
+namespace allergies
+{
 
-}  // namespace allergies
+    class allergy_test
+    {
+    public:
+        allergy_test(int i);
 
-#endif  // ALLERGIES_H
+        bool is_allergic_to(std::string);
+
+        std::unordered_set<std::string> get_allergies();
+
+    private:
+        std::unordered_set<std::string> allergies_;
+    };
+} // namespace allergies
+
+#endif // ALLERGIES_H
