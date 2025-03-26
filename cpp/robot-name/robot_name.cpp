7a7781945bbbc29361f19b475d229dc43bf541a3
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-26 14:23:07 +0100

    Added robot-name exercise

---
+++ cpp/robot-name/robot_name.cpp
@@ -0,0 +1,7 @@
+#include "robot_name.h"
+
+namespace robot_name {
+
+// TODO: add your solution here
+
+}  // namespace robot_name
---
+++ cpp/robot-name/robot_name.h
@@ -0,0 +1,10 @@
+#if !defined(ROBOT_NAME_H)
+#define ROBOT_NAME_H
+
+namespace robot_name {
+
+// TODO: add your solution here
+
+}  // namespace robot_name
+
+#endif  // ROBOT_NAME_H
