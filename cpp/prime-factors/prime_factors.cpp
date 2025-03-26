add94efef2b6df3c1607856c076289c2c6929c07
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-26 13:32:03 +0100

    Added prime-factors exercise

---
+++ cpp/prime-factors/prime_factors.cpp
@@ -0,0 +1,7 @@
+#include "prime_factors.h"
+
+namespace prime_factors {
+
+// TODO: add your solution here
+
+}  // namespace prime_factors
---
+++ cpp/prime-factors/prime_factors.h
@@ -0,0 +1,10 @@
+#if !defined(PRIME_FACTORS_H)
+#define PRIME_FACTORS_H
+
+namespace prime_factors {
+
+// TODO: add your solution here
+
+}  // namespace prime_factors
+
+#endif  // PRIME_FACTORS_H
