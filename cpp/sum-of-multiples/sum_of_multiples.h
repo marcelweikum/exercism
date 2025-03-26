dbd95285da01434e99951daf7aa21e31bb4f98f5
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-26 09:34:14 +0100

    Added sum-of-multiples exercise

---
+++ cpp/sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,10 @@
+#if !defined(SUM_OF_MULTIPLES_H)
+#define SUM_OF_MULTIPLES_H
+
+namespace sum_of_multiples {
+
+// TODO: add your solution here
+
+}  // namespace sum_of_multiples
+
+#endif  // SUM_OF_MULTIPLES_H
