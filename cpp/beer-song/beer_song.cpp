8ce22f02f060a49195bc5c81d203d3a0d4aebfbc
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-27 11:47:21 +0100

    Added beer-song exercise

---
+++ cpp/beer-song/beer_song.cpp
@@ -0,0 +1,7 @@
+#include "beer_song.h"
+
+namespace beer_song {
+
+// TODO: add your solution here
+
+}  // namespace beer_song
---
+++ cpp/beer-song/beer_song.h
@@ -0,0 +1,10 @@
+#if !defined(BEER_SONG_H)
+#define BEER_SONG_H
+
+namespace beer_song {
+
+// TODO: add your solution here
+
+}  // namespace beer_song
+
+#endif  // BEER_SONG_H
