23dfe9738824275b9af5f5647910252cf901cf6d
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-27 13:14:20 +0100

    Added atbash-cipher exercise

---
+++ cpp/atbash-cipher/atbash_cipher.cpp
@@ -0,0 +1,7 @@
+#include "atbash_cipher.h"
+
+namespace atbash_cipher {
+
+// TODO: add your solution here
+
+}  // namespace atbash_cipher
---
+++ cpp/atbash-cipher/atbash_cipher.h
@@ -0,0 +1,10 @@
+#if !defined(ATBASH_CIPHER_H)
+#define ATBASH_CIPHER_H
+
+namespace atbash_cipher {
+
+// TODO: add your solution here
+
+}  // namespace atbash_cipher
+
+#endif  // ATBASH_CIPHER_H
