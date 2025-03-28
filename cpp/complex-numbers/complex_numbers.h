5a3965ea27b3aff5e58fda4ee00d96a3e3880e81
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-28 09:51:53 +0100

    Added complex-numbers exercise

---
+++ cpp/complex-numbers/complex_numbers.h
@@ -0,0 +1,10 @@
+#if !defined(COMPLEX_NUMBERS_H)
+#define COMPLEX_NUMBERS_H
+
+namespace complex_numbers {
+
+// TODO: add your solution here
+
+}  // namespace complex_numbers
+
+#endif  // COMPLEX_NUMBERS_H
