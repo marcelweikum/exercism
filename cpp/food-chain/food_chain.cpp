087af60cdd4fc3e4c9762adb31571457cd478b35
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-28 12:41:24 +0100

    Added food-chain exercise

---
+++ cpp/food-chain/food_chain.cpp
@@ -0,0 +1,7 @@
+#include "food_chain.h"
+
+namespace food_chain {
+
+// TODO: add your solution here
+
+}  // namespace food_chain
---
+++ cpp/food-chain/food_chain.h
@@ -0,0 +1,10 @@
+#if !defined(FOOD_CHAIN_H)
+#define FOOD_CHAIN_H
+
+namespace food_chain {
+
+// TODO: add your solution here
+
+}  // namespace food_chain
+
+#endif  // FOOD_CHAIN_H
