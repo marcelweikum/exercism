fbe965c4b8fa2665a68fbc77fb8f0872c418d7fb
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-28 14:44:18 +0100

    Added word-count exercise

---
+++ cpp/word-count/word_count.cpp
@@ -0,0 +1,7 @@
+#include "word_count.h"
+
+namespace word_count {
+
+// TODO: add your solution here
+
+}  // namespace word_count
---
+++ cpp/word-count/word_count.h
@@ -0,0 +1,10 @@
+#if !defined(WORD_COUNT_H)
+#define WORD_COUNT_H
+
+namespace word_count {
+
+// TODO: add your solution here
+
+}  // namespace word_count
+
+#endif  // WORD_COUNT_H
