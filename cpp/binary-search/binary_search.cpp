175e693acd815d78829dbf77e581827d163fdd75
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-28 08:25:23 +0100

    Added binary-search exercise

---
+++ cpp/binary-search/binary_search.cpp
@@ -0,0 +1,7 @@
+#include "binary_search.h"
+
+namespace binary_search {
+
+// TODO: add your solution here
+
+}  // namespace binary_search
---
+++ cpp/binary-search/binary_search.h
@@ -0,0 +1,10 @@
+#if !defined(BINARY_SEARCH_H)
+#define BINARY_SEARCH_H
+
+namespace binary_search {
+
+// TODO: add your solution here
+
+}  // namespace binary_search
+
+#endif  // BINARY_SEARCH_H
