d001329e675e334145a22c6195d2d12bfeb09e89
marcelweikum <marcel.weikum@wesodev.de>
2025-03-31 15:20:30 +0200

    Added binary-search-tree exercise

---
+++ cpp/binary-search-tree/binary_search_tree.h
@@ -0,0 +1,10 @@
+#if !defined(BINARY_SEARCH_TREE_H)
+#define BINARY_SEARCH_TREE_H
+
+namespace binary_search_tree {
+
+// TODO: add your solution here
+
+}  // namespace binary_search_tree
+
+#endif  // BINARY_SEARCH_TREE_H
