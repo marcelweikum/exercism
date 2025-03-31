b620df6878494626bac49c2aac21d410dee3cc45
Marcel Weikum <marcel.weikum@wesodev.de>
2025-03-31 11:36:22 +0200

    Solved etl exercise

--- cpp/etl/etl.cpp
+++ cpp/etl/etl.cpp
@@ -1,7 +1,22 @@
 #include "etl.h"
 
-namespace etl {
+#include <cctype>
 
-// TODO: add your solution here
+namespace etl
+{
 
-}  // namespace etl
+    std::map<char, int> transform(std::map<int, std::vector<char>> old_map)
+    {
+        std::map<char, int> new_map;
+
+        for (const auto &[score, letters] : old_map)
+        {
+            for (char letter : letters)
+            {
+                new_map[std::tolower(letter)] = score;
+            }
+        }
+        return new_map;
+    }
+
+} // namespace etl
--- cpp/etl/etl.h
+++ cpp/etl/etl.h
@@ -1,10 +1,14 @@
 #if !defined(ETL_H)
 #define ETL_H
 
-namespace etl {
+#include <map>
+#include <vector>
 
-// TODO: add your solution here
+namespace etl
+{
 
-}  // namespace etl
+    std::map<char, int> transform(std::map<int, std::vector<char>> old_map);
 
-#endif  // ETL_H
+} // namespace etl
+
+#endif // ETL_H
