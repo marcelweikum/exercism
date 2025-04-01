d52a9c684ea4f23815946c0b94a5a289a10750f0
marcelweikum <marcel.weikum@wesodev.de>
2025-04-01 12:54:21 +0200

    Added binary exercise

---
+++ cpp/binary/binary.cpp
@@ -0,0 +1,7 @@
+#include "binary.h"
+
+namespace binary {
+
+// TODO: add your solution here
+
+}  // namespace binary
---
+++ cpp/binary/binary.h
@@ -0,0 +1,10 @@
+#if !defined(BINARY_H)
+#define BINARY_H
+
+namespace binary {
+
+// TODO: add your solution here
+
+}  // namespace binary
+
+#endif  // BINARY_H
