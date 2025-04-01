83a52b275e27593004fa891db1ff17a651614861
marcelweikum <marcel.weikum@wesodev.de>
2025-04-01 12:39:54 +0200

    Added armstrong-numbers exercise

---
+++ cpp/armstrong-numbers/armstrong_numbers.cpp
@@ -0,0 +1,7 @@
+#include "armstrong_numbers.h"
+
+namespace armstrong_numbers {
+
+// TODO: add your solution here
+
+}  // namespace armstrong_numbers
---
+++ cpp/armstrong-numbers/armstrong_numbers.h
@@ -0,0 +1,10 @@
+#if !defined(ARMSTRONG_NUMBERS_H)
+#define ARMSTRONG_NUMBERS_H
+
+namespace armstrong_numbers {
+
+// TODO: add your solution here
+
+}  // namespace armstrong_numbers
+
+#endif  // ARMSTRONG_NUMBERS_H
