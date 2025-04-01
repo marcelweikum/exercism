7b80ee610a3736f4445085047f8c9a1b4fc14a99
marcelweikum <marcel.weikum@wesodev.de>
2025-04-01 13:03:21 +0200

    Added hexadecimal exercise

---
+++ cpp/hexadecimal/hexadecimal.cpp
@@ -0,0 +1,7 @@
+#include "hexadecimal.h"
+
+namespace hexadecimal {
+
+// TODO: add your solution here
+
+}  // namespace hexadecimal
---
+++ cpp/hexadecimal/hexadecimal.h
@@ -0,0 +1,10 @@
+#if !defined(HEXADECIMAL_H)
+#define HEXADECIMAL_H
+
+namespace hexadecimal {
+
+// TODO: add your solution here
+
+}  // namespace hexadecimal
+
+#endif  // HEXADECIMAL_H
