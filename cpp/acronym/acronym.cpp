70f06e773c611c0be05df64b208c5e1519e57c66
marcelweikum <marcel.weikum@wesodev.de>
2025-04-01 12:21:15 +0200

    Added acronym exercise

---
+++ cpp/acronym/acronym.cpp
@@ -0,0 +1,7 @@
+#include "acronym.h"
+
+namespace acronym {
+
+// TODO: add your solution here
+
+}  // namespace acronym
---
+++ cpp/acronym/acronym.h
@@ -0,0 +1,10 @@
+#if !defined(ACRONYM_H)
+#define ACRONYM_H
+
+namespace acronym {
+
+// TODO: add your solution here
+
+}  // namespace acronym
+
+#endif  // ACRONYM_H
