abf40e560998be09ba3bccb63b7a0cc954276610
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 11:16:03 +0200

    Added all-your-base exercise

---
+++ cpp/all-your-base/all_your_base.cpp
@@ -0,0 +1,7 @@
+#include "all_your_base.h"
+
+namespace all_your_base {
+
+// TODO: add your solution here
+
+}  // namespace all_your_base
---
+++ cpp/all-your-base/all_your_base.h
@@ -0,0 +1,10 @@
+#if !defined(ALL_YOUR_BASE_H)
+#define ALL_YOUR_BASE_H
+
+namespace all_your_base {
+
+// TODO: add your solution here
+
+}  // namespace all_your_base
+
+#endif  // ALL_YOUR_BASE_H
