c0c0cad1c29fe32d3fe75996bcc5b9f16991418a
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 13:41:09 +0200

    Added darts exercise

---
+++ cpp/darts/darts.cpp
@@ -0,0 +1,7 @@
+#include "darts.h"
+
+namespace darts {
+
+// TODO: add your solution here
+
+}  // namespace darts
---
+++ cpp/darts/darts.h
@@ -0,0 +1,10 @@
+#if !defined(DARTS_H)
+#define DARTS_H
+
+namespace darts {
+
+// TODO: add your solution here
+
+}  // namespace darts
+
+#endif  // DARTS_H
