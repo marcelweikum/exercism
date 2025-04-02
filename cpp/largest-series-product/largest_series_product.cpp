490135a18cf5ae1dfae911b1b2303ea9f0b733b7
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 13:54:09 +0200

    Added largest-series-product exercise

---
+++ cpp/largest-series-product/largest_series_product.cpp
@@ -0,0 +1,7 @@
+#include "largest_series_product.h"
+
+namespace largest_series_product {
+
+// TODO: add your solution here
+
+}  // namespace largest_series_product
---
+++ cpp/largest-series-product/largest_series_product.h
@@ -0,0 +1,10 @@
+#if !defined(LARGEST_SERIES_PRODUCT_H)
+#define LARGEST_SERIES_PRODUCT_H
+
+namespace largest_series_product {
+
+// TODO: add your solution here
+
+}  // namespace largest_series_product
+
+#endif  // LARGEST_SERIES_PRODUCT_H
