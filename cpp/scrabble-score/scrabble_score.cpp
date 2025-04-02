f04174c075ed403d6b6cae58618867d9ed412459
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 11:05:00 +0200

    Added scrabble-score exercise

---
+++ cpp/scrabble-score/scrabble_score.cpp
@@ -0,0 +1,7 @@
+#include "scrabble_score.h"
+
+namespace scrabble_score {
+
+// TODO: add your solution here
+
+}  // namespace scrabble_score
---
+++ cpp/scrabble-score/scrabble_score.h
@@ -0,0 +1,10 @@
+#if !defined(SCRABBLE_SCORE_H)
+#define SCRABBLE_SCORE_H
+
+namespace scrabble_score {
+
+// TODO: add your solution here
+
+}  // namespace scrabble_score
+
+#endif  // SCRABBLE_SCORE_H
