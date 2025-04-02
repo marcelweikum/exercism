cdf93ec246e5f493a744caa13a327de050d8e0c0
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 11:58:55 +0200

    Added robot-simulator exercise

---
+++ cpp/robot-simulator/robot_simulator.h
@@ -0,0 +1,10 @@
+#if !defined(ROBOT_SIMULATOR_H)
+#define ROBOT_SIMULATOR_H
+
+namespace robot_simulator {
+
+// TODO: add your solution here
+
+}  // namespace robot_simulator
+
+#endif  // ROBOT_SIMULATOR_H
