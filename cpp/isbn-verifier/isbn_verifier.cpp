38ada8c120f5df0f5d140ac341f5054930226d14
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 14:12:54 +0200

    Added isbn-verifier exercise

---
+++ cpp/isbn-verifier/isbn_verifier.cpp
@@ -0,0 +1,7 @@
+#include "isbn_verifier.h"
+
+namespace isbn_verifier {
+
+// TODO: add your solution here
+
+}  // namespace isbn_verifier
---
+++ cpp/isbn-verifier/isbn_verifier.h
@@ -0,0 +1,10 @@
+#if !defined(ISBN_VERIFIER_H)
+#define ISBN_VERIFIER_H
+
+namespace isbn_verifier {
+
+// TODO: add your solution here
+
+}  // namespace isbn_verifier
+
+#endif  // ISBN_VERIFIER_H
