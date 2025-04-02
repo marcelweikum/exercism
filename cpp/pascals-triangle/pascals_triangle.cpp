df1bd358070ee7cf77c049e81bc438a8bdcd602e
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-02 10:05:41 +0200

    Added pascals-triangle exercise

---
+++ cpp/pascals-triangle/pascals_triangle.cpp
@@ -0,0 +1,7 @@
+#include "pascals_triangle.h"
+
+namespace pascals_triangle {
+
+// TODO: add your solution here
+
+}  // namespace pascals_triangle
---
+++ cpp/pascals-triangle/pascals_triangle.h
@@ -0,0 +1,10 @@
+#if !defined(PASCALS_TRIANGLE_H)
+#define PASCALS_TRIANGLE_H
+
+namespace pascals_triangle {
+
+// TODO: add your solution here
+
+}  // namespace pascals_triangle
+
+#endif  // PASCALS_TRIANGLE_H
