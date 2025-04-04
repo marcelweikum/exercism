b28d4ae0b1abcff29c047950ba0df91c69496afd
Marcel Weikum <marcel.weikum@wesodev.de>
2025-04-04 15:21:40 +0200

    Solved list-ops exercise

--- cpp/list-ops/list_ops.cpp
+++ cpp/list-ops/list_ops.cpp
@@ -1,7 +1,19 @@
 #include "list_ops.h"
 
-namespace list_ops {
+namespace list_ops
+{
 
-// TODO: add your solution here
+    void append(std::vector<int> &f_list, std::vector<int> &s_list)
+    {
+        for (int n : s_list)
+        {
+            f_list.push_back(n);
+        }
+    }
 
-}  // namespace list_ops
+    size_t length(std::vector<int> list)
+    {
+        return list.size();
+    }
+
+} // namespace list_ops
--- cpp/list-ops/list_ops.h
+++ cpp/list-ops/list_ops.h
@@ -1,7 +1,82 @@
 #pragma once
 
-namespace list_ops {
+#include <vector>
+#include <cstddef>
 
-// TODO: add your solution here
+namespace list_ops
+{
 
-}  // namespace list_ops
+    void append(std::vector<int> &f_list, std::vector<int> &s_list);
+
+    template <typename T>
+    auto concat(const std::vector<T> &list)
+    {
+        std::vector<typename T::value_type> result;
+
+        for (const auto &sublist : list)
+        {
+            result.insert(result.end(), sublist.begin(), sublist.end());
+        }
+
+        return result;
+    }
+
+    template <typename T, typename Func>
+    std::vector<T> filter(const std::vector<T> &input, Func predicate)
+    {
+        std::vector<T> result;
+        for (const auto &el : input)
+        {
+            if (predicate(el))
+            {
+                result.push_back(el);
+            }
+        }
+        return result;
+    }
+
+    size_t length(std::vector<int> list);
+
+    template <typename T, typename Func>
+    auto map(const std::vector<T> &input, Func func)
+    {
+        using ReturnType = decltype(func(input[0]));
+        std::vector<ReturnType> result;
+        result.reserve(input.size());
+
+        for (const auto &el : input)
+        {
+            result.push_back(func(el));
+        }
+
+        return result;
+    }
+
+    template <typename T, typename Acc, typename Func>
+    Acc foldl(const std::vector<T> &input, Acc init, Func func)
+    {
+        for (const auto &el : input)
+        {
+            init = func(init, el);
+        }
+        return init;
+    }
+
+    template <typename T, typename Acc, typename Func>
+    Acc foldr(const std::vector<T> &input, Acc init, Func func)
+    {
+        for (auto it = input.rbegin(); it != input.rend(); ++it)
+        {
+            init = func(init, *it);
+        }
+        return init;
+    }
+
+    template <typename T>
+    std::vector<T> reverse(const std::vector<T> &input)
+    {
+        std::vector<T> result(input.rbegin(), input.rend());
+        return result;
+    }
+
+} // namespace list_ops
