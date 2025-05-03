4bd35556d39aee67020d21dff73ab1ada7243ab8
marcelweikum <marcel.weikum@wesodev.de>
2025-05-03 11:28:19 +0200

    Added kindergarten-garden exercise

---
+++ cpp/kindergarten-garden/kindergarten_garden.cpp
@@ -0,0 +1,7 @@
+#include "kindergarten_garden.h"
+
+namespace kindergarten_garden {
+
+// TODO: add your solution here
+
+}  // namespace kindergarten_garden
---
+++ cpp/kindergarten-garden/kindergarten_garden.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace kindergarten_garden {
+
+// TODO: add your solution here
+
+}  // namespace kindergarten_garden
