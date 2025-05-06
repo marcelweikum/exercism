4ff7453834450747a62e6ef465bfb810228d162b
marcelweikum <marcel.weikum@wesodev.de>
2025-05-06 11:38:54 +0200

    Added dnd-character exercise

---
+++ cpp/dnd-character/dnd_character.cpp
@@ -0,0 +1,7 @@
+#include "dnd_character.h"
+
+namespace dnd_character {
+
+// TODO: add your solution here
+
+}  // namespace dnd_character
---
+++ cpp/dnd-character/dnd_character.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace dnd_character {
+
+// TODO: add your solution here
+
+}  // namespace dnd_character
